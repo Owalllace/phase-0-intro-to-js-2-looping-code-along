ed110d55a013482496a18867ea915745a3904d77
OliviaWallace <oliviawallacepdx@gmail.com>
2024-01-01 16:41:50 -0800

    my file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards() {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++){
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return thankYouMessages;
+}
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
