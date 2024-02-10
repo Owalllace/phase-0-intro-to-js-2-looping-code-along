77095690da4b241f8ee921d7fc511f970a700534
OliviaWallace <oliviawallacepdx@gmail.com>
2024-02-10 11:11:39 -0800

    done

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+// use for loops when you know how many times you want the loop to run
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["Teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i= 0; i< gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    };
+}
+wrapGifts(gifts);
+
 const names = ["Guadalupe", "Ollie", "Aki"];
-function writeCards() {
+const event = "surprise";
+
+function writeCards(names, event) {
     const thankYouMessages = [];
-    for (let i = 0; i < names.length; i++){
-        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        );
     }
     return thankYouMessages;
-}
-function countDown(number) {
-    for (let i = number; i >= 0; i--) {
-        console.log(i);
+};
+
+writeCards();
+
+function countDown(startingNumber) {
+    let count = startingNumber;
+    while (count >= 0) {
+    console.log(count);
+    count--;
     }
 }
